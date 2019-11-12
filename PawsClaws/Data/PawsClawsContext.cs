e61590bcce7509e9d5205f2209d8682c5a4d8f71
Justin Carnes <justincarnes0@gmail.com>
2019-11-12 02:12:21 -0600

    Major work on the Pet page tonight.
    -Finished setting up the model for Pets.
    
    -Used this model to build scaffolding and create the database. Now we have a working database and webpages to add, remove, edit, and view the details of pets contained within.
    
    -Added some of my current and past pets as test values.
    
    -Layout has improved a lot but it still needs a lot of work. (Someone please save me from this nightmare of trying to center the Pet containers)
    
    -Still need to figure out how I'm gonna implement images.

---
+++ PawsClaws/Data/PawsClawsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PawsClaws.Models;
+
+namespace PawsClaws.Data
+{
+    public class PawsClawsContext : DbContext
+    {
+        public PawsClawsContext (DbContextOptions<PawsClawsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PawsClaws.Models.Pet> Pet { get; set; }
+    }
+}
---
+++ PawsClaws/Pages/Pets/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PawsClaws.Data;
+using PawsClaws.Models;
+
+namespace PawsClaws.Pages.Pets
+{
+    public class CreateModel : PageModel
+    {
+        private readonly PawsClaws.Data.PawsClawsContext _context;
+
+        public CreateModel(PawsClaws.Data.PawsClawsContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Pet Pet { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Pet.Add(Pet);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
