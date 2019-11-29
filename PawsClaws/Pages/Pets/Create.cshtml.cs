3b84855991cd6ba9540639066a0dfb778d1ab150
Justin Carnes <justincarnes0@gmail.com>
2019-11-29 02:17:32 -0600

    IMAGE SUPPORT. When creating a new pet, one or more images of said pet can now be uploaded. Supports .png and .jpg. You can see some of my tests using photos of Jeb Bush.
    -Todo: display the images on the pets page
    -Todo: delete image directories when pets are deleted
    -Todo: possibly add a reference string to the database? I looked into this but it's kinda complex, and the way I structured the directories should make things easy enough to find without modifying the database schema.

--- PawsClaws/Pages/Pets/Create.cshtml.cs
+++ PawsClaws/Pages/Pets/Create.cshtml.cs
@@ -2,9 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.IO;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
 using PawsClaws.Data;
 using PawsClaws.Models;
 
@@ -13,10 +21,12 @@ namespace PawsClaws.Pages.Pets
     public class CreateModel : PageModel
     {
         private readonly PawsClaws.Data.PawsClawsContext _context;
+        private readonly IHostingEnvironment _environment;
 
-        public CreateModel(PawsClaws.Data.PawsClawsContext context)
+        public CreateModel(PawsClaws.Data.PawsClawsContext context, IHostingEnvironment ihe)
         {
             _context = context;
+            _environment = ihe;
         }
 
         public IActionResult OnGet()
@@ -37,6 +47,40 @@ namespace PawsClaws.Pages.Pets
             _context.Pet.Add(Pet);
             await _context.SaveChangesAsync();
 
+            var newFileName = string.Empty;
+
+            if (HttpContext.Request.Form.Files != null)
+            {
+                var fileName = "";
+                string PathDB = "";
+
+                var files = HttpContext.Request.Form.Files;
+                int i = 0;
+
+                foreach(var file in files)
+                {
+                    if (file.Length > 0)
+                    {
+                        fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                        var FileExtension = Path.GetExtension(fileName);
+
+                        newFileName = i++ + FileExtension;
+
+                        fileName = Path.Combine(_environment.WebRootPath, "images", "" + Pet.ID) + $@"\{newFileName}";
+
+                        PathDB = "images/" + Pet.ID + "/" + newFileName;
+
+                        Directory.CreateDirectory(Path.GetDirectoryName(fileName));
+                        using (FileStream fs = System.IO.File.Create(fileName))
+                        {
+                            file.CopyTo(fs);
+                            fs.Flush();
+                        }
+                    }
+                }
+            }
+
+            await _context.SaveChangesAsync();
             return RedirectToPage("./Index");
         }
     }
