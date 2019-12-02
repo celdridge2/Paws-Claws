3af445a8fc76957ac9b80f3ab62694444f6b082f
Justin Carnes <justincarnes0@gmail.com>
2019-12-02 01:45:34 -0600

    Login support.
    -Added support for user authentication in the Startup.cs file. A user that is not logged in is granted access to all pages except the Create, Edit, and Delete pet pages. In the future we can add more pages that only authenticated users can access pretty easily.
    
    -Added a Login page that accepts username and password. Currently there is just a single account "NPHEmployees" and a single, unchangeable password. Of course we would like to be able to change the password, so I'll fix that in the future. Password is stored in a hashed form and compared against the hash of the input from the password field. Right now the hash of the correct password is hard-coded, but that will change when I add support for password changes.
    
    -TODO: Make certain elements appear/disappear when logged in/logged out.
    
    -TODO: Polish everything. Really this is just about the end of the road (for v1).

---
+++ PawsClaws/Pages/Login.cshtml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PawsClaws.Pages
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public LoginData loginData { get; set; }
+
+        private string TrueHash = "49CUNrfTtTKX1YGsb3pIj550JVAun08s4wFNjCRbAfg=";
+
+        public async Task<PageResult> OnPostAsync()
+        {
+            loginData.HashPass();
+            if (ModelState.IsValid)
+            {
+                var isValid = (loginData.Username == "NPHEmployees" && loginData.PasswordHash == TrueHash);
+                if (!isValid)
+                {
+                    ModelState.AddModelError("", "Username or Password is invalid!");
+                    return Page();
+                }
+                var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
+                identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, loginData.Username));
+                identity.AddClaim(new Claim(ClaimTypes.Name, loginData.Username));
+                var principal = new ClaimsPrincipal(identity);
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, new AuthenticationProperties { IsPersistent = loginData.RememberMe });
+                RedirectToPage("Index");
+                return Page();
+            }
+            else
+            {
+                ModelState.AddModelError("", "Username and Password cannot be left blank.");
+                return Page();
+            }
+        }
+
+        public class LoginData
+        {
+            [Required]
+            public string Username { get; set; }
+
+            [Required, DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            public string PasswordHash { get; private set; }
+
+            public bool RememberMe { get; set; }
+
+            public void HashPass()
+            {
+                PasswordHash = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                    password: Password,
+                    salt: Convert.FromBase64String("ET7MYwwotuHk1+wZOeycog=="),
+                    prf: KeyDerivationPrf.HMACSHA1,
+                    iterationCount: 10000,
+                    numBytesRequested: 256 / 8));
+            }
+        }
+    }
+}
---
+++ PawsClaws/Pages/Logout.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PawsClaws.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        public async Task<IActionResult> OnGetAsync()
+        {
+            HttpContext.SignOutAsync();
+            return RedirectToPage("Pets/Index");
+        }
+    }
+}
